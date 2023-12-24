184bb952dfb8284b0b9477d594961043e10dfeff
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2023-12-24 18:18:04 +0300

    Load data

--- app/src/main/java/com/bengisusahin/cryptocurrencyk/view/MainActivity.kt
+++ app/src/main/java/com/bengisusahin/cryptocurrencyk/view/MainActivity.kt
@@ -3,13 +3,57 @@ package com.bengisusahin.cryptocurrencyk.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bengisusahin.cryptocurrencyk.R
+import com.bengisusahin.cryptocurrencyk.model.CryptoModel
+import com.bengisusahin.cryptocurrencyk.service.CryptoAPI
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+
+    private val BASE_URL = "https://raw.githubusercontent.com/"
+    private var cryptoModels: ArrayList<CryptoModel>? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         //https://raw.githubusercontent.com/atilsamancioglu/K21-JSONDataSet/master/crypto.json
 
+        loadData()
+    }
+
+    private fun loadData(){
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        //API ile reorfiti birbirine bağlıcaz
+        val service = retrofit.create(CryptoAPI::class.java)
+        val call = service.getData()
+
+        call.enqueue(object : Callback<List<CryptoModel>>{
+            override fun onResponse(
+                call: Call<List<CryptoModel>>,
+                response: Response<List<CryptoModel>>
+            ) {
+                if (response.isSuccessful){
+                    response.body()?.let {
+                        //eğerki bu boş gelmediyse bunu yap demek
+                        cryptoModels = ArrayList(it)
+                        for (cryptoModel : CryptoModel in cryptoModels!!){
+                            println(cryptoModel.currency)
+                            println(cryptoModel.price)
+                        }
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<List<CryptoModel>>, t: Throwable) {
+                t.printStackTrace()            }
+
+        })
     }
 }
